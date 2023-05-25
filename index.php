0abb26b62bcdeb018965dd67fba4e0839e8a6ac2
NoobMaster4269 <134286591+NoobMaster4269@users.noreply.github.com>
2023-05-25 10:52:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<body>
+    <h1>Température à ont-de-Marsans</h1>
+<?php
+
+class MyDB extends SQLite3 {
+    function  __construct(){
+        $this->open('bdd.db');
+    }
+}
+$db = new MyDB();
+
+$sql = "SELECT * FROM Entries";
+$result = $db->query($sql);
+
+    echo '<table>';
+
+    $doublerow = true;
+
+while ($row = $result->fetchArray()) {
+    echo '<tr>';
+
+    $columnCount = count($row);
+
+    for ($i = 0; $i < $columnCount; $i++) {
+
+        echo '<td>'.$row[$i].'</td>';
+    }
+    
+    echo '</tr>';
+
+    $doublerow = false;
+}
+echo '</table>';
+    
+$db->close();
+?>
+
+</body>
+</html>
