6eeebe7a6b5aaf3d4f58645c35bc29dc195a3f6a
Antonin PONS <antoninpons@protonmail.com>
2023-05-26 18:15:36 +0200

    commit

--- graph.py
+++ graph.py
@@ -5,6 +5,7 @@ import sqlite3
 
 bdd = sqlite3.connect("bdd.db")
 cur = bdd.cursor()
+dic = {"temp": 1, "feels_like": 2, "temp_min": 3, "temp_max": 4, "pressure": 5, "humidity": 6, "weather": 8, "wind_speed": 9, "wind_deg": 10}
 
 def time_to_int(time):
     return int(time.split(':')[0])*60+int(time.split(':')[1])
@@ -14,9 +15,10 @@ def int_to_time(time):
 
 def get_time_by_hour(day: str, column: str, delta: int):
     cur_hour = bdd.cursor()
-    entries = cur_hour.execute("SELECT * FROM Entries WHERE date='"+day+"';")
+    hour_values = cur_hour.execute("SELECT * FROM Entries WHERE date='"+day+"';")
+    col = dic[column]
     values = []
-    for i in entries:
+    for i in hour_values:
         values.append((i[12], i[col]))
 
     i = 0
@@ -50,7 +52,7 @@ def get_time_by_hour(day: str, column: str, delta: int):
 
 def get_day_moy(day, column):
     cur_moy = bdd.cursor()
-    col = 1
+    col = dic[column]
     day_values = cur_moy.execute("SELECT * FROM Entries WHERE date='"+day+"';")
     somme = 0
     l = 0
@@ -76,7 +78,7 @@ def get_time_by_day(unit, type_unit, column):
     plt.plot(days, data)
     plt.show()
 
-#get_time_by_hour("2023-05-26", 'temp', 60)
-get_time_by_day('05', 'month', 'temp')
+get_time_by_hour("2023-05-26", 'temp', 60)
+#get_time_by_day('05', 'month', 'temp')
 
 bdd.close()
