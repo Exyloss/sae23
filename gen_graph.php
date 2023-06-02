7a601501f6bcc92e6e1652ceaf5f3165b50c3731
Antonin PONS <antoninpons@protonmail.com>
2023-06-02 12:20:55 +0200

    modif bdd

--- gen_graph.php
+++ gen_graph.php
@@ -5,5 +5,6 @@ $sid = session_id();
 $message = exec("/usr/bin/python3 graph.py --file ".$sid.".png");
 $data = file_get_contents($sid.".png");
 $_SESSION['graph'] = base64_encode($data);
+exec("/bin/rm ".$sid)
 header("location: graph.php");
 ?>
