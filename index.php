8127806b9f771d21ccf5d849a332c0be943532a9
BASTOSTERONE <127217848+BASTOSTERONE@users.noreply.github.com>
2023-06-08 14:47:40 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,19 +8,37 @@ include('db_class.php');
     <meta charset="UTF-8">
     <title>Graphiques</title>
 </head>
-<body background="Images/imgaefondnuage.jpg">
+<?php
+
+
+$sql = "SELECT weather FROM Entries  WHERE id = '".$donnees['id']."' ";
+$results = $db->query($sql);
+$row = $results->fetchArray();
+
+
+$sql2 = "SELECT Image FROM Images WHERE idImage = '".$row['weather']."' ";
+$results2 = $db->query($sql2);
+$row2 = $results2->fetchArray();
+
+?>
+<style>
+body {
+    background-image : url(<?php echo '"'.$row2['Image'].'"'; ?> )
+}
+</style>
+<body>
     <h1>Générer des graphiques</h1>
     <form method="GET" action="gen_graph.php">
-        <h2>Pelotter un jour précis</h2>
+        <h2>Plotter un jour précis</h2>
         <input type="date" name="day"></input> <br>
         <input type="text" name="delta"/>
-        <h2>Pelotter un mois du type YYYY-MM</h2>
+        <h2>Plotter un mois du type YYYY-MM</h2>
         <input type="month" name="month" />
-        <h2>Pelotter une année</h2>
+        <h2>Plotter une année</h2>
         <input type="year" name="year" />
-        <h2>Pelotter quel champ ?</h2>
+        <h2>Plotter quel champ ?</h2>
         <select name="champ">
-
+        
 <?php
 
 $db = new MyDB();
@@ -39,34 +57,7 @@ while ($donnees=$reponse->fetchArray())
         <input type="submit"/>
     </form>
 
-<?php
 
-
-$sql = "SELECT weather FROM Entries  WHERE date = ";
-$results = $db->query($sql);
-$row = $results->fetchArray();
-
-
-$sql2 = "SELECT * FROM Images";
-$results2 = $db->query($sql2);
-$row2 = $results2->fetchArray();
-
-if ($row == $row2['weather']){
-    echo '<img src=" '.$row2['Image'].'></img>'.'<br>';
-}
-else{
-
-}
-
-
-if (isset($_SESSION['graph'])) {
-    echo '<img src="data:image/png;base64, '.$_SESSION['graph'].'"/>';
-}
-/*
-echo '<img src=" '.$row2['Image'].'></img>'.'<br>';
-*/
-
-?>
 
 
 </body>
